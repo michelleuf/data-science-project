1dfe3cfc2a533a798b7da938e90aeef6adee3a22
ov1n <thenukaovin@gmail.com>
2022-01-23 21:19:40 +0530

    Initialized project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import * as d3 from 'd3';
+import * from './'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+class App extends React.Component {
+  constructor(props){
+     super(props);
+     this.myRef = React.createRef(); 
+  }
+  componentDidMount(){
+     console.log(this.myRef);
+     d3.select(this.myRef.current)
+     .append('p')
+     .text('Hello d3');
+  }
+  render(){
+   return (
+     <div ref={this.myRef}>
+     </div>
+   );
+  }
+  
+ }
+ export default App;
